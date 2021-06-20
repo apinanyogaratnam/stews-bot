7ce079d9e247bc40fa95f2650cd75acf6c0cefe7
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-20 18:18:06 +0000

    adding  command with reddit api and getting top 50 posts from 'memes' subreddit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import random
 from keep_alive import keep_alive
 from inspire_command import contains_sad_words, contains_emoji
+import praw
 
 sad_words = ["kms", "down bad", "depressed", "sad"]
 encouraging_words = [
@@ -60,7 +61,7 @@ async def on_message(message):
   if message.content.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
-  if not contains_sad_words() or not contains_emoji():
+  if contains_sad_words(msg) and not contains_emoji(msg):
       await message.channel.send(random.choice(encouraging_words))
   
   if msg.startswith('$help'):
@@ -71,6 +72,29 @@ async def on_message(message):
       "Here are the different keywords:\n" + 
       "kms, down bad, depressed, sad (gives a positive message)"
     )
+
+  if msg.startswith('$meme'):
+      reddit = praw.Reddit(client_id = os.environ['REDDIT_CLIENT_ID'],
+                          client_secret = os.environ['REDDIT_CLIENT_SECRET'],
+                          username = os.environ['REDDIT_USERNAME'],
+                          password = os.environ['REDDIT_PASSWORD'],
+                          user_agent = "stews_bot")
+
+      all_subreddits = []
+      reddit = reddit.subreddit("memes")
+      top = subreddit.top(limit = 50)
+
+      for post in top:
+        all_subreddits.append(post)
+
+      random_sub = random.choice(all_subreddits)
+
+      name = random_sub.title
+      url = random_sub.url
+      embed = discord.Embed(title = name)
+      
+      embed.add_image(url = url)
+      await message.channel.send(embed = embed)
 
 keep_alive()
 
