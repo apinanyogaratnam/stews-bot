5538d76c1f67b56a3e887265c97ab3d09d69ef56
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-26 23:42:13 +0000

    fixed errors in training.py file but still tf doesn't work because of model fit error

--- training.py
+++ training.py
@@ -24,8 +24,8 @@ for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern) # tokenize splits the sentence into words ex. hey i am john would be hey, i, am, john
 
-        words.append(word_list)
-        documents.extend((word_list, intent['tag']))
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
@@ -57,22 +57,22 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype='object')
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 # build neural network
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),)), activiation='relu')
+model.add(Dense(128, input_shape=(len(train_x[0]),)), activation='relu')
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical crossentropy', optimizer = sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.fit(np.array(train_x, dtype='object'), np.array(train_y, dtype='object'), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.model')
 print("Done")
