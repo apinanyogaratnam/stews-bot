80ffe07fda57ca3d03a818ac8b883c4950b8576e
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-26 22:41:16 +0000

    completed neural network

--- training.py
+++ training.py
@@ -63,3 +63,16 @@ train_x = list(training[:, 0])
 train_y = list(training[:, 1])
 
 # build neural network
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),)), activiation='relu')
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical crossentropy', optimizer = sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
