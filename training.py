7d457db13a5f05ec42279d5dbc8472253799de17
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-26 23:46:06 +0000

    fixed cannot convert to int model.fit() error

--- training.py
+++ training.py
@@ -64,15 +64,15 @@ train_y = list(training[:, 1])
 
 # build neural network
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),)), activation='relu')
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
 sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical crossentropy', optimizer = sgd, metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer = sgd, metrics=['accuracy'])
 
-model.fit(np.array(train_x, dtype='object'), np.array(train_y, dtype='object'), epochs=200, batch_size=5, verbose=1)
+model.fit(np.asarray(train_x).astype(np.int), np.asarray(train_y).astype(np.int), epochs=200, batch_size=5, verbose=1)
 model.save('chatbot_model.model')
 print("Done")
