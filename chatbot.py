939c5f87ad93c9e1acb050973fe7c35bae26bbc3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-06-26 20:23:32 -0400

    fixed issues with chatbot.py and currently responds accordingly (needs to run training.py to make model, then run chatbot.py)

--- chatbot.py
+++ chatbot.py
@@ -8,11 +8,11 @@ from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('intents.json')).read()
+intents = json.loads(open('intents.json').read())
 
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
-model = load_model('chatbot_model.model')
+model = load_model('chatbotmodel.h5')
 
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
@@ -34,7 +34,7 @@ def bag_of_words(sentence):
 
 def predict_class(sentence):
     bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
+    res = model.predict(np.asarray([bow]).astype(np.int))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
 
@@ -60,7 +60,7 @@ def get_response(intents_list, intents_json):
 print("Bot is running!")
 
 while True:
-    message = input("")
+    message = input("input: ")
     ints = predict_class(message)
     res = get_response(ints, intents)
     print(res)
