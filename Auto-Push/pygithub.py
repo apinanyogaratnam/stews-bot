ec480d490e1b2ac8d38adc3720ed3de74a6ffaf4
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-09 04:54:27 +0000

    added text to test_file.txt and changes to pygithub.py of removing unnecessary lines of code

--- Auto-Push/pygithub.py
+++ Auto-Push/pygithub.py
@@ -9,15 +9,14 @@ def main():
     # when in sub dir:
     os.chdir('..')
     import subprocess
-    commit_message = "testing out commit using subprocess (1)"
+    commit_message = "changes to test_file.txt"
     subprocess.call(['git', 'add', '.'])
     subprocess.call(['git', 'commit', '-m', '{}'.format(commit_message)])
     token = os.environ['TOKEN']
     username = "apinanyogaratnam"
-    repo_name = "automate_git"
+    repo_name = "Stews-Bot"
     subprocess.call(['git', 'push', 'https://{}@github.com/{}/{}.git'.format(token, username, repo_name)])
 
 
 if __name__ == '__main__':
-    # main()
     main()
