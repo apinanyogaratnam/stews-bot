ada672598d8f76560efb1cf42b15c331001d20d3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-07-10 17:08:00 +0000

    completed several functions for birthday_notifier

--- birthday_notifier.py
+++ birthday_notifier.py
@@ -1,11 +1,47 @@
-from datetime import date
-today = date.today()
+from date_object import date, current_date
+from replit import db
 
-d1 = today.strftime("%d/%m/%Y") # 16/09/2019
-temp_lst = d1.split("/")
 
-day = temp_lst[0]
-month = temp_lst[1]
-year = temp_lst[2]
+def is_valid_format(possible_date_string):
+    lst = possible_date_string.split("/")
 
-def add_birthday(username, date):
+    if len(lst) != 3: return False
+    if not lst[0].isdigit() or not lst[1].isdigit() or not lst[2].isdigit(): return False
+
+    day = int(lst[0])
+    month = int(lst[1])
+    year = int(lst[2])
+
+    if day < 0 or day > 31: return False
+    if month < 0 or month > 12: return False
+    if year < 0 or year > current_date().year: return False
+
+    return True
+
+
+def format_string_to_date_object(date_string):
+    # date_string is as xx/xx/xxxx
+    # (day)/(month)/(year)
+    # assuming date_string is a valid date_string
+    lst = date_string.split("/")
+    date_obj = date(lst[0], lst[1], lst[2])
+    
+    return date_obj
+
+
+def add_birthday(username, date): # username in discord, date object
+    # if already exists, birthday gets replaced
+    db[username] = date
+
+
+def get_birthday(username):
+    return db[username]
+
+
+def remove_birthday(username):
+    del db[username]
+
+
+def print_all_users():
+    for item in db.keys():
+        print(item)
--- date_object.py
+++ date_object.py
@@ -3,3 +3,19 @@ class date:
         self.day = day
         self.month = month
         self.year = year
+    
+
+def current_date():
+    from datetime import date
+    today = date.today()
+
+    d1 = today.strftime("%d/%m/%Y") # 16/09/2019
+    temp_lst = d1.split("/")
+
+    day = temp_lst[0]
+    month = temp_lst[1]
+    year = temp_lst[2]
+
+    current_date_object = date(day, month, year)
+
+    return current_date_object
