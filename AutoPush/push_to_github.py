7887b56117a33bec9b00cb7905e43b6223a08f8b
stewietheangel <apinanapinan@icloud.com>
2021-07-14 00:42:06 +0000

    changes made

--- AutoPush/push_to_github.py
+++ AutoPush/push_to_github.py
@@ -7,7 +7,7 @@ def push(time_to_sleep, filename):
         os.system('git config user.email "{}"'.format(email))
         os.system('touch AutoPush/{}'.format(filename))
         os.system('python3 AutoPush/pygithub.py')
-        time.sleep(time_to_sleep)
+        # time.sleep(time_to_sleep)
         os.system('rm AutoPush/{}'.format(filename))
         os.system('python3 AutoPush/pygithub.py')
         time.sleep(time_to_sleep)
