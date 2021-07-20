c1c066c47b130726da2b5b491678656716d73abb
stewietheangel <apinanapinan@icloud.com>
2021-07-20 22:19:25 +0000

    changes made

--- main.py
+++ main.py
@@ -68,14 +68,16 @@ async def on_message(message):
     # if msg.lower() in " sadge ":
     #     await message.channel.send(":sadge:")
     if (message.content == ("api")):
-        voiceChannel = discord.utils.get(client.guild.voice_channels, name="vc-general")
-        voice = discord.utils.get(client.voice_clients, guild=client.guild)
+        server_id = 750477536353583205
+        guild = client.get_guild(server_id)
+        voiceChannel = discord.utils.get(guild.voice_channels, name="vc-general")
+        voice = discord.utils.get(client.voice_clients, guild=guild)
         if not voice.is_connected():
             await voiceChannel.connect()
 
 
-    if (str(message.author) == "vai9er#5526"):
-        await message.channel.send("-play grubhub delivery dance")
+    # if (str(message.author) == "vai9er#5526"):
+    #     await message.channel.send("-play grubhub delivery dance")
 
     if msg.startswith("$birthday "):
         # adding a birthday to database
